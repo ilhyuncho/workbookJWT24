3f91a27e4cda29e6780b4d1766c005f0b215e1a4
choilhyun <cihg155@gmail.com>
2024-08-09 17:26:20 +0900

    [ Authorization 전송 페이지] nginx 서버를 실행하고 http://localhost/apiLogin.html 를 실행했을때 문제 해결

--- src/main/java/com/example/workbookjwt2024/config/CustomSecurityConfig.java
+++ src/main/java/com/example/workbookjwt2024/config/CustomSecurityConfig.java
@@ -23,6 +23,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -93,9 +98,28 @@ public class CustomSecurityConfig {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);    // 세션 사용 안함
 
+        // cors 문제 해결 ( 브라우저의 응답 메시지에 해당 호출이 문제 없었다는 해더 정보들을 같이 전송하는 방식
+        // nginx 서버를 실행하고 http://localhost/apiLogin.html 를 실행했을때 문제 해결
+        http.cors(httpSecurityCorsConfigurer -> {
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+        });
+
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 
